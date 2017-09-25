71da4aebe78f6c33d31d3581d2c816f6ae18749b
Hakan Akgül <hakanakgul34@gmail.com>
2017-09-25 22:00:34 +0300

    created at 07.08.2017 (dd.mm.yyyy)

---
+++ 20170807-6Con_SayiTahminOyunu/Program.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _20170807_6Con_SayiTahminOyunu
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            /*
+             bilgisayarın tuttuğu 1-100 arasındaki sayıyı, klavyeden girerek tahmin edeceğimiz bir uygulama yazınız. kaç seferde bulduğunu söyleyiniz.
+             */
+
+            // bilgisayarın kendisi rastgele bir sayı üretecekse Random sınıfı kullanılabilir.
+            Random r = new Random();
+            int tutulan = r.Next(1, 100);
+            int tahmin = 0, sayac = 0;
+            Console.WriteLine("Aklımdan bir sayı tuttum.. Haydi tahmin et ! ");
+            Console.WriteLine("---------------------------------------------");
+            Console.Write("Tahminim : ");
+            do
+            {
+                if (int.TryParse(Console.ReadLine(),out tahmin) == true)
+                {
+                    sayac++;
+                    if (tahmin > tutulan)
+                    {
+                        Console.WriteLine("AŞAĞI");
+                        Console.Write("Tahminim : ");
+                    }
+                    else if(tahmin < tutulan)
+                    {
+                        Console.WriteLine("YUKARI");
+                        Console.Write("Tahminim : ");
+                    }
+                }
+                else
+                {
+                    Console.WriteLine("GİRDİĞİN ŞEY SAYI OLSUN !");
+                    Console.Write("Tahminim : ");
+                }
+
+            } while (tahmin != tutulan);
+
+            Console.Write("Tebrikler {0} sayısını {1}. denemede buldun",tutulan,sayac);
+          
+            Console.Read();
+
+        }
+    }
+}
